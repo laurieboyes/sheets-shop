b9999aabce603da8f23a06e88fa8d2906ad6c0d2
Laurie Boyes <laurie.boyes@ft.com>
2018-07-07 12:16:01 +0100

    get sheets stuff

--- handler.ts
+++ handler.ts
@@ -1,11 +1,28 @@
-export function hello(event, context, callback) {
+const queryString = require('querystring');
+import fetch from 'node-fetch';
+
+async function getStuff() {
+  const spreadsheetId = '1A67bs-DAERPQBBPzIqszfQgUpoZuji4axrIio1UoP4E';
+  const q = queryString.stringify({
+    key: process.env.SHEETS_API_KEY,
+    ranges: 'Big fan!A1:B2'
+  });
+
+  return await (await fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values:batchGet?${q}`
+  )).json();
+}
+
+export async function hello(event, context, callback) {
+  const rawStuff = await getStuff();
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: 'got my stuff',
+      rawStuff
+    })
   };
 
   callback(null, response);
-};
+}
