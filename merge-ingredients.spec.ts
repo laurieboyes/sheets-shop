447cb0b0cbd2d85ca93d120b9c2ebde0a1aaed43
Laurie Boyes <laurie.boyes@ft.com>
2018-07-08 21:45:13 +0100

    units (as long as they're the same)

--- merge-ingredients.spec.ts
+++ merge-ingredients.spec.ts
@@ -1,7 +1,12 @@
 import mergeIngredients from './merge-ingredients';
 
 describe('merge-ingredients', () => {
-  it('merge plurals and singles where s is the plural form', () => {
+  it('should merge plurals and singles where s is the plural form', () => {
     expect(mergeIngredients(['1 lemon', '2 lemons'])).toEqual(['3 lemon']);
+  });
+  it('should work with units', () => {
+    expect(mergeIngredients(['100ml mango juice', '50ml mango juice'])).toEqual(
+      ['150ml mango juice']
+    );
   });
 });
--- merge-ingredients.ts
+++ merge-ingredients.ts
@@ -1,8 +1,24 @@
+function parseQuantity(quantityStr) {
+  if (quantityStr.match(/^\d+$/)) {
+    return {
+      amount: +quantityStr
+    };
+  } else if (/^\d+[a-z]+$/.test(quantityStr)) {
+    const matches = quantityStr.match(/^(\d+)([a-z]+)$/);
+    return {
+      unit: matches[2],
+      amount: +matches[1]
+    };
+  } else {
+    throw new Error('Wth is this quantity? ' + quantityStr);
+  }
+}
+
 export default function(ingredients) {
   const mergedList = [];
 
   ingredients.forEach(ingredientStr => {
-    const quantity = +ingredientStr.split(' ')[0];
+    const quantity = parseQuantity(ingredientStr.split(' ')[0]);
     const itemName = ingredientStr
       .split(' ')
       .slice(1)
@@ -22,15 +38,15 @@ export default function(ingredients) {
       // if it's not already in there, stick it in
       mergedList.push({
         name: itemName,
-        quantity
+        ...quantity
       });
     } else {
       // else, inc the quantity
-      itemAlreadyMerged.quantity += quantity;
+      itemAlreadyMerged.amount += quantity.amount;
     }
   });
 
   return mergedList.map(item => {
-    return `${item.quantity} ${item.name}`;
+    return `${item.amount}${item.unit ? item.unit : ''} ${item.name}`;
   });
 }
